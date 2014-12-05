14238c1fa14e86c2f2352e5d031af7b6ec964e3d
Rastislav Lamos <lamos.rasto@gmail.com>
2014-12-05 13:33:54 +0100

    added post listing in The Loop

--- lib/structure/loops.php
+++ lib/structure/loops.php
@@ -3,17 +3,38 @@
  * @ WebVision.sk 2014
  */
 
-function lamosty_do_post_loop() {
+function lamosty_do_post_loop()
+{
     if (have_posts()) :
-        while(have_posts()) : the_post();
+        while (have_posts()) {
+            the_post();
+?>
+            <article class="blog-post">
+                <header class="page-header">
+                    <h2 class="blog-post-title">
+                        <a href="<?php the_permalink(); ?>">
+                            <?php the_title(); ?>
+                        </a>
+                    </h2>
+                    <p class="blog-post-meta"><?php echo get_the_date(); ?>
+                        by <strong><?php the_author(); ?></strong> |
+                        <a href="<?php the_permalink(); ?>#comments">
+                            <?php comments_number(); ?>
+                        </a>
+                    </p>
+                </header>
 
-            the_title();
+                <div class="excerpt">
+                    <?php the_excerpt(); ?>
+                </div>
 
-            the_category();
+                <a href="<?php the_permalink(); ?>">Read More...</a>
+            </article><!-- /.blog-post -->
 
-            the_content();
 
-        endwhile;
+
+<?php
+        }
 
     endif;
 
